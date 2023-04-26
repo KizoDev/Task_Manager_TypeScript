95eb1127867241267c2b47adae8fd08d12c83c4b
kingsley Arinze <kingsleyarizona39@gmail.com>
2023-04-26 05:27:46 +0100

    completed routes

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -6,9 +6,21 @@ import Task from '../model/task';
 const getTasks = async(req: Request, res: Response): Promise<void> => {
  try {
     const tasks: ITask[] = await Task.find();
-    res.status(200).json({tasks})
+    res.json({
+      status:201,
+      message:'All tasks available',
+      successful:true,
+      task: tasks, 
+    })
  } catch (error) {
-    throw error
+   res.json({
+      status:401,
+      message:' no Task added',
+      successful:true,
+      task: null, 
+      tasks: null
+    })
+   
  }
 }
 
@@ -53,10 +65,21 @@ const updateTask =  async(req: Request, res: Response): Promise<void> => {
         body
        )
        const allTasks: ITask[] = await Task.find();
-       res.status(200).json({message: 'Task updated', task: updateTask, tasks: allTasks })
+       res.json({
+         status:201,
+         message:'Task updated',
+         successful:true,
+         task: updateTask, 
+         tasks: allTasks
+       })
     } catch (error) {
-      throw error  
-    }
+      res.json({
+         status:401,
+         message:' update failed',
+         successful:true,
+         task: null, 
+         tasks: null
+       })    }
 }
 
 // delete Task
@@ -64,10 +87,23 @@ const deleteTask  = async (req: Request, res: Response): Promise<void> => {
  try {
     const deletedTask: ITask | null = await Task.findByIdAndRemove(req.params.id);
     const allTasks = await Task.find();
-    res.status(200).json({message: 'Task deleted', task: deletedTask, tasks: allTasks})
- } catch (error) {
-    throw error
- }
+    res.json({
+      status:201,
+      message:'Task deleted',
+      successful:true,
+      task: deletedTask, 
+      tasks: allTasks
+    })
+    } catch (error) {
+      res.json({
+         status:401,
+         message:' update failed',
+         successful:true,
+         task: null, 
+         tasks: null
+       })
+      
+   }
 }
 
 
